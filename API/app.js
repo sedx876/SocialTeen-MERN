22d8f19fa023d802b3773a16238891f622546475
sedx876 <swatkins876@gmail.com>
2021-06-07 10:24:49 -0400

    create-react-app spinup for client frontend

--- API/app.js
+++ API/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const app = express()
 const mongoose = require('mongoose')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
@@ -10,7 +11,6 @@ const dotenv = require('dotenv')
 const chalk = require('chalk')
 const colors = require('colors')
 
-const app = express()
 dotenv.config()
 
 // configure db
@@ -24,28 +24,55 @@ mongoose.connection.on('error', err => {
   console.table(`DB connection error: ${err.message}`)
 })
 
+//Bring in Routes
+const postRoutes = require('./routes/post')
+const authRoutes = require('./routes/auth')
+const userRoutes = require('./routes/user')
+// apiDocs
+app.get('/api', (req, res) => {
+  fs.readFile('docs/apiDocs.json', (err, data) => {
+    if (err) {
+      res.status(400).json({
+        error: err
+    })
+  }
+    const docs = JSON.parse(data)
+    res.json(docs)
+    })
+})
+
 //Middleware
 const morganMiddleware = morgan(function (tokens, req, res) {
-    return [
-      '\n\n\n',
-      chalk.hex('#ff4757').bold('💩🍄💩  Morgan --> '),
-      chalk.hex('#34ace0').bold(tokens.method(req, res)),
-      chalk.hex('#ffb142').bold(tokens.status(req, res)),
-      chalk.hex('#ff5252').bold(tokens.url(req, res)),
-      chalk.hex('#2ed573').bold(tokens['response-time'](req, res) + ' ms'),
-      chalk.hex('#f78fb3').bold('@ ' + tokens.date(req, res)),
-      chalk.yellow(tokens['remote-addr'](req, res)),
-      chalk.hex('#fffa65').bold('from ' + tokens.referrer(req, res)),
-      chalk.hex('#1e90ff')(tokens['user-agent'](req, res)),
-      '\n\n\n',
-    ].join(' ')
-  })
+  return [
+    '\n\n\n',
+    chalk.hex('#ff4757').bold('💩🍄💩  Morgan --> '),
+    chalk.hex('#34ace0').bold(tokens.method(req, res)),
+    chalk.hex('#ffb142').bold(tokens.status(req, res)),
+    chalk.hex('#ff5252').bold(tokens.url(req, res)),
+    chalk.hex('#2ed573').bold(tokens['response-time'](req, res) + ' ms'),
+    chalk.hex('#f78fb3').bold('@ ' + tokens.date(req, res)),
+    chalk.yellow(tokens['remote-addr'](req, res)),
+    chalk.hex('#fffa65').bold('from ' + tokens.referrer(req, res)),
+    chalk.hex('#1e90ff')(tokens['user-agent'](req, res)),
+    '\n\n\n',
+  ].join(' ')
+})
 
-  app.use(morganMiddleware)
+app.use(morganMiddleware)
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(expressValidator())
+app.use(cors())
+app.use('/api', postRoutes)
+app.use('/api', authRoutes)
+app.use('/api', userRoutes)
+app.use(function(err, req, res, next) {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).json({ error: 'Unauthorized!' })
+  }
+})
 
-
-
-  const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080
 
 app.listen(port, () => 
 {console.log(`NODE API is listening on port ${port}`.brightCyan.inverse)})
