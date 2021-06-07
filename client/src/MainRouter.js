90aaf3b7cdc85b781764fabb0f7fc625816e573c
sedx876 <swatkins876@gmail.com>
2021-06-07 10:40:09 -0400

    Added MAinRouter.js

---
+++ client/src/MainRouter.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+
+
+const MainRouter = () => {
+  return (
+    <div>
+			<Menu/>
+			<Switch>
+
+			</Switch>
+    </div>
+    )
+}
+
+export default MainRouter
