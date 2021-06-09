b1a45e512d96bc87d49977a5d47042b10e1706c7
sedx876 <swatkins876@gmail.com>
2021-06-09 10:56:18 -0400

    Added EditPost.js

--- client/src/post/Posts.js
+++ client/src/post/Posts.js
@@ -37,54 +37,54 @@ class Posts extends Component{
   }
 
 	renderPosts = posts => {
-		return(
-			<div>
-				{posts.map((post, i) =>{
-					const posterId = post.postedBy
-					? `/user/${post.postedBy._id}`
-					: "";
-					const posterName = post.postedBy
-					? post.postedBy.name
-					: " Unknown";
-					return(
-						<div key={i}>
-							<div>
-							<img
-                src={`${
-                  process.env.REACT_APP_API_URL
-                  }/post/photo/${post._id}`}
-										alt={post.title}
-										onError={i =>
+    return (
+      <div className="row">
+        {posts.map((post, i) => {
+          const posterId = post.postedBy
+            ? `/user/${post.postedBy._id}`
+            : "";
+          const posterName = post.postedBy
+            ? post.postedBy.name
+            : " Unknown";
+            return (
+              <div className="card col-md-4 mb-2" key={i}>
+                <div className="card-body">
+                  <img
+                    src={`${
+                      process.env.REACT_APP_API_URL
+                    }/post/photo/${post._id}`}
+											alt={post.title}
+											onError={i =>
                     (i.target.src = `${DefaultPost}`)
-                  }
-                  className=""
-                  style={{ height: "200px", width: "100%" }}
-              />
-							<h5>{post.title}</h5>
-							<p className="card-text">
-                {post.body.substring(0, 100)}
-              </p>
-							<br/>
-							<p className="">
-                Posted by{" "}
-                <Link to={`${posterId}`}>
-                  {posterName}{" "}
-                </Link>
-                  on {new Date(post.created).toDateString()}
-              </p>
-							<Link
-                to={`/post/${post._id}`}
-                className="btn btn-raised btn-primary btn-sm"
-              >
-                Read more
-              </Link>
-							</div>
-						</div>
-					)
-				})}
-			</div>
-		)
-	}
+                    }
+                    className="img-thunbnail mb-3"
+                    style={{ height: "200px", width: "100%" }}
+                  />
+                    <h5 className="card-title">{post.title}</h5>
+                    <p className="card-text">
+                      {post.body.substring(0, 100)}
+                    </p>
+                    <br />
+                    <p className="font-italic mark">
+                      Posted by{" "}
+                    <Link to={`${posterId}`}>
+                      {posterName}{" "}
+                    </Link>
+                      on {new Date(post.created).toDateString()}
+                    </p>
+                    <Link
+                      to={`/post/${post._id}`}
+                      className="btn btn-raised btn-primary btn-sm"
+                    >
+                      Read more
+                    </Link>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+        )
+    }
 
 	render(){
 		const { posts, page } = this.state;
